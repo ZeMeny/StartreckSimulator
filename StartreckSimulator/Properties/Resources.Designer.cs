a692962a54aa9be22b6c7700e3f6304340a4c73b
ZeMeny <35948457+ZeMeny@users.noreply.github.com>
2020-10-18 15:54:57 +0300

    added schema to the repo

--- StartreckSimulator/Properties/Resources.Designer.cs
+++ StartreckSimulator/Properties/Resources.Designer.cs
@@ -8,10 +8,10 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace StartreckSimulator.Properties
-{
-
-
+namespace StartreckSimulator.Properties {
+    using System;
+    
+    
     /// <summary>
     ///   A strongly-typed resource class, for looking up localized strings, etc.
     /// </summary>
@@ -19,52 +19,65 @@ namespace StartreckSimulator.Properties
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    internal class Resources
-    {
-
+    internal class Resources {
+        
         private static global::System.Resources.ResourceManager resourceMan;
-
+        
         private static global::System.Globalization.CultureInfo resourceCulture;
-
+        
         [global::System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
-        internal Resources()
-        {
+        internal Resources() {
         }
-
+        
         /// <summary>
         ///   Returns the cached ResourceManager instance used by this class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Resources.ResourceManager ResourceManager
-        {
-            get
-            {
-                if ((resourceMan == null))
-                {
+        internal static global::System.Resources.ResourceManager ResourceManager {
+            get {
+                if (object.ReferenceEquals(resourceMan, null)) {
                     global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("StartreckSimulator.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
             }
         }
-
+        
         /// <summary>
         ///   Overrides the current thread's CurrentUICulture property for all
         ///   resource lookups using this strongly typed resource class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Globalization.CultureInfo Culture
-        {
-            get
-            {
+        internal static global::System.Globalization.CultureInfo Culture {
+            get {
                 return resourceCulture;
             }
-            set
-            {
+            set {
                 resourceCulture = value;
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to {
+        ///    &quot;$schema&quot;: &quot;http://json-schema.org/draft-07/schema#&quot;,
+        ///    &quot;$id&quot;: &quot;http://example.com/product.schema.json&quot;,
+        ///    &quot;title&quot;: &quot;mrs-startrek classification schema&quot;,
+        ///    &quot;definitions&quot;: {
+        ///        &quot;request&quot;: {
+        ///            &quot;description&quot;: &quot;A classification request from mrs to startrek&quot;,
+        ///            &quot;title&quot;: &quot;classification request message&quot;,
+        ///            &quot;type&quot;: &quot;object&quot;,
+        ///            &quot;properties&quot;: {
+        ///                &quot;command&quot;:{
+        ///                    &quot;description&quot;: &quot;The request command&quot;,
+        ///                    [rest of string was truncated]&quot;;.
+        /// </summary>
+        internal static string mrs_classification_schema {
+            get {
+                return ResourceManager.GetString("mrs_classification_schema", resourceCulture);
             }
         }
     }
